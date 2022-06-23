b10af52ea998e77c188cae2a4af7c772d6163185
Lucas Bitencourt <lblukz@aol.com>
2022-06-22 21:49:44 -0300

    Requisito 3 concluído
    Adiciona CSS para melhor visualização da página durante o desenvolvimento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Login.css';
 import { connect } from 'react-redux';
 import { login } from '../actions';
 // import { Redirect } from 'react-router-dom';
@@ -38,7 +37,7 @@ class Login extends React.Component {
   render() {
     const { buttonDisabled, email, password } = this.state;
     return (
-      <div>
+      <div className="login">
         <form>
           <input
             name="email"
