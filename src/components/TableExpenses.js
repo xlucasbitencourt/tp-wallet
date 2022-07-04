491db5af52122eac16574ff62890f28d735ef584
Lucas Bitencourt <lblukz@aol.com>
2022-07-04 20:29:23 -0300

    Ajusta visualização da tabela para mobile

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -12,47 +12,90 @@ class TableExpenses extends Component {
   render() {
     const { expenses } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses.map((item) => (
+                <tr key={ item.id }>
+                  <td>{item.description}</td>
+                  <td>{item.tag}</td>
+                  <td>{item.method}</td>
+                  <td>{parseFloat(item.value).toFixed(2)}</td>
+                  <td>{item.exchangeRates[item.currency].name}</td>
+                  <td>{parseFloat(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+                  <td>
+                    {(item.value * item.exchangeRates[item.currency].ask).toFixed(2)}
+                  </td>
+                  <td>Real</td>
+                  <td>
+                    <button
+                      data-testid="delete-btn"
+                      type="button"
+                      name="excluir"
+                      onClick={ () => this.delete(expenses, item.id) }
+                    >
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        <div className="expenses-list">
+          <h2>Despesas</h2>
           {
-            expenses.map((item) => (
-              <tr key={ item.id }>
-                <td>{item.description}</td>
-                <td>{item.tag}</td>
-                <td>{item.method}</td>
-                <td>{parseFloat(item.value).toFixed(2)}</td>
-                <td>{item.exchangeRates[item.currency].name}</td>
-                <td>{parseFloat(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
-                <td>{(item.value * item.exchangeRates[item.currency].ask).toFixed(2)}</td>
-                <td>Real</td>
-                <td>
-                  <button
-                    data-testid="delete-btn"
-                    type="button"
-                    name="excluir"
-                    onClick={ () => this.delete(expenses, item.id) }
-                  >
-                    Excluir
-                  </button>
-                </td>
-              </tr>
+            expenses.map((it) => (
+              <div className="expense-mobile" key={ it.id }>
+                <span className="title">Descrição: </span>
+                <span>{it.description}</span>
+                <br />
+                <span className="title">Tag: </span>
+                <span>{it.tag}</span>
+                <br />
+                <span className="title">Método de pagamento: </span>
+                <span>{it.method}</span>
+                <br />
+                <span className="title">Valor: </span>
+                <span>{parseFloat(it.value).toFixed(2)}</span>
+                <br />
+                <span className="title">Moeda: </span>
+                <span>{it.exchangeRates[it.currency].name}</span>
+                <br />
+                <span className="title">Câmbio utilizado: </span>
+                <span>{parseFloat(it.exchangeRates[it.currency].ask).toFixed(2)}</span>
+                <br />
+                <span className="title">Valor convertido: </span>
+                <span>{(it.value * it.exchangeRates[it.currency].ask).toFixed(2)}</span>
+                <br />
+                <span className="title">Moeda de conversão: </span>
+                <span>Real</span>
+                <br />
+                <button
+                  type="button"
+                  onClick={ () => this.delete(expenses, it.id) }
+                >
+                  Excluir
+                </button>
+              </div>
             ))
           }
-        </tbody>
-      </table>
+        </div>
+      </>
     );
   }
 }
